0271da25636343f350aaaa828026ab357de63942
Jonathan Fok kan <jfokkan@gmail.com>
2015-04-17 17:44:50 -0400

    ADDED help message and a few operators

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,30 @@
 
 import Data.List
 import System.Environment
+import Control.Monad (when)
 
-solveRPN :: (Num a, Read a) => [String] -> a
+solveRPN :: [String] -> Float
 solveRPN = head . foldl foldingFunc []
     where foldingFunc (x:y:ys) "*" = x * y:ys
-          --foldingFunc (x:y:ys) "/" = x / y:ys
+          foldingFunc (x:y:ys) "/" = x / y:ys
           foldingFunc (x:y:ys) "-" = x - y:ys
           foldingFunc (x:y:ys) "+" = x + y:ys
+          foldingFunc xs "sum" = [sum xs]
+          foldingFunc (x:xs) "ln" = log x:xs
+          foldingFunc (x:y:ys) "^" = (y ** x):ys
           foldingFunc xs numStr = read numStr:xs
 
 main = do
   args <- getArgs
   if null args then
-      putStrLn "rpn-calc\nReverse Polish Notation Calculator"
+      do
+        putStrLn "Reverse Polish Notation Calculator"
+        putStrLn "Operators suported: * / - + sum ln ^"
+        putStrLn "Options:"
+        putStrLn "\t--help: for help message"
   else
-      print $ solveRPN args
+      do
+        when (head args == "--help") $ do
+          putStrLn "Operators suported: * / - + sum ln ^"
+          putStrLn "Don't forget to escape \\* when using command line"
+        print $ solveRPN args
